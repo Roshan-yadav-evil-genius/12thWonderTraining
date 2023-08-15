646867306dbea75d82db9f0722384c8d6267c552
Roshan yadav <roshanyadav1st@gmail.com>
2023-08-15 16:08:37 +0530

    updated

---
+++ CollectionsConcept/Program.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionsConcept
+{
+    public class User : IComparable<User>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public User(int id, string name, string email)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+        }
+
+        public int CompareTo(User other)
+        {
+            if (this.Id < other.Id)
+            {
+                return -1;
+            }
+            else if (this.Id > other.Id)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+    class CompareNames : IComparer<User>
+    {
+        public int Compare(User x, User y)
+        {
+            if (x.Id < y.Id)
+            {
+                return -1;
+            }
+            else if (x.Id > y.Id)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+
+    class OperationOn<T>
+    {
+
+        public void Add(T a, T b)
+        {
+            dynamic num1 = a;
+            dynamic num2 = b;
+            Console.Write("Addition Output = ");
+            Console.WriteLine(num1 + num2);
+        }
+    }
+    internal class Program
+    {
+        public static int CompareByName(User s1, User s2)
+        {
+            return s1.Name.CompareTo(s2.Name);
+        }
+
+        static void Main(string[] args)
+        {
+            Hashtable ht = new Hashtable();
+            ht.Add("name", "Roshan Yadav");
+            ht.Add("Age", 22);
+            ht.Add("Salary", 20000.0);
+
+            Console.WriteLine(ht["name"]);
+            Console.WriteLine("Name".GetHashCode());
+            Console.WriteLine("name".GetHashCode());
+
+            foreach (object key in ht.Keys)
+            {
+                Console.WriteLine(key + " " + ht[key]);
+            }
+
+
+            //GenericUriParser Collections
+            Console.WriteLine("-----------List Generic------------");
+
+            List<User> list = new List<User>() {
+                new User(1, "john", "john@gmail.com") ,
+                new User(2, "amit", "amit@gmail.com")
+            };
+            list.Add(new User(3, "master", "master@gmail.com"));
+            list.Add(new User(4, "main", "main@gmail.com"));
+            list.Add(new User(5, "teacher", "teacher@gmail.com"));
+            list.Add(new User(6, "accoutant", "accoutant@gmail.com"));
+
+            Console.WriteLine("List Count : " + list.Count);
+            Console.WriteLine(list[2]);
+            list.Insert(2, new User(7, "worker", "teacher@gmail.com"));
+            list.Remove(list[2]);
+
+            foreach (User user in list)
+            {
+                Console.WriteLine(user.Id + " " + user.Name + " " + user.Email);
+            }
+
+            //Generics 
+            OperationOn<int> op = new OperationOn<int>();
+            op.Add(2, 4);
+
+            //Dictionary
+            Console.WriteLine("-----------Dictionary------------");
+            Dictionary<string, object> dic = new Dictionary<string, object>();
+            dic.Add("name", "Guddu");
+            dic.Add("email", "Guddu@gmail.com");
+            dic.Add("PhnNo", 8476868560);
+
+            foreach (var key in dic.Keys)
+            {
+                Console.WriteLine(key + " " + dic[key]);
+            }
+
+            Console.WriteLine("-----------IComparable------------");
+            List<int> marks = new List<int>() { 40, 20, 90, 10, 30 };
+            Console.Write("List : ");
+            marks.ForEach(x => Console.Write(x + " "));
+            Console.WriteLine();
+            marks.Sort();
+            Console.Write("Sorted List : ");
+            marks.ForEach(x => Console.Write(x + " "));
+            Console.WriteLine();
+
+            List<User> users = new List<User>() {
+                new User(5, "john", "john@gmail.com") ,
+                new User(2, "amit", "amit@gmail.com") ,
+                new User(9, "shark", "shark@gmail.com") ,
+                new User(0, "master", "master@gmail.com")};
+            Console.Write("List : ");
+            users.ForEach(x => Console.Write(x.Id + " "));
+            Console.WriteLine();
+            users.Sort();
+            Console.Write("Sorted List : ");
+            users.ForEach(x => Console.Write(x.Id + " "));
+            Console.WriteLine(); 
+            Console.Write("Reversed List : ");
+            users.Reverse();
+            users.ForEach(x => Console.Write(x.Id + " "));
+            Console.WriteLine("\n[+] Comparing BY Names");
+            CompareNames obj = new CompareNames();
+            users.Sort(obj);
+            Console.Write("Sorted List : ");
+            users.ForEach(x => Console.Write(x.Id + " "));
+            users.Reverse();
+            Console.WriteLine("\n[+] By Using 4th overload");
+            users.Sort(1,2,obj);
+            Console.Write("Sorted List : ");
+            users.ForEach(x => Console.Write(x.Id + " "));
+            Console.WriteLine("\n[+] By Using 2nd overload (Delegate)");
+            Comparison<User> compareDelegate = new Comparison<User>(CompareByName);
+            //types is only allowed to pass as parameter not method to overcome this we use delegate
+            users.Sort(compareDelegate);
+            Console.Write("Sorted List : ");
+            users.ForEach(x => Console.Write(x.Name + " "));
+            //Byte Anonymous Method
+            users.Sort(delegate (User s1,User s2) { return s1.Name.CompareTo(s2.Name); });
+            Console.Write("\nSorted List : ");
+            users.ForEach(x => Console.Write(x.Name + " "));
+            // By LAmbda function
+            users.Sort(( s1, s2)=>s1.Name.CompareTo(s2.Name));
+            Console.Write("\nSorted List : ");
+            users.ForEach(x => Console.Write(x.Name + " "));
+            // By Direct passing methodname
+            users.Sort(CompareByName);
+            Console.Write("\nSorted List : ");
+            users.ForEach(x => Console.Write(x.Name + " "));
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ LINIQLearnings/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINIQLearnings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Language integrated Query
+            // random unshorted array
+            Console.WriteLine("------------Without LINQ----------");
+            int[] numbers = { 21, 3, 4, 5, 6, 7, 8, 9, 10, 1 };
+            int count = 0;
+            foreach (int i in numbers)
+            {
+                if (i > 5)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
+            int[] brr = new int[count];
+            int index = 0;
+            foreach (int i in numbers)
+            {
+                if (i > 5)
+                {
+                    brr[index] = i;
+                    index++;
+                }
+            }
+            Array.Sort(brr);
+            Array.Reverse(brr);
+            foreach (var item in brr) Console.Write(item + " ");
+
+            Console.WriteLine("\n------------With LINQ----------");
+            // from i in arr select i;
+            // from i in arr where i>5 select i;
+            // from i in arr where i>5 orderby i select i;
+            // from i in arr where i>5 orderby i descending select i;
+
+            var myarr = from i in numbers where i > 5 orderby i descending select i;
+
+            foreach (var item in myarr) Console.Write(item + " ");
+
+            // LINQ to sql
+            //enable-migrations
+            //add-migration CreateAnimalTable
+            //update-database
+            //using (var context = new AnimalContext())
+            //{
+            //    var animalsToAdd = new[]
+            //    {
+            //    new Animal { name = "Lion", location = "Africa" },
+            //    new Animal { name = "Tiger", location = "India" },
+            //    new Animal { name = "Elephant", location = "Africa" },
+            //    new Animal { name = "Panda", location = "China" },
+            //    new Animal { name = "Kangaroo", location = "Australia" }
+            //};
+
+            //    context.Animals.AddRange(animalsToAdd);
+
+            //    // Save changes to the database
+            //    context.SaveChanges();
+            //}
+            Console.WriteLine("Inserted 5 dummy rows into the Animals table.");
+            using (var context = new AnimalContext())
+            {
+                // Retrieve a single entity by its primary key (id)
+                var animal = context.Animals.Find(2);
+
+                // Retrieve multiple entities using LINQ queries
+                var animalsInAfrica = context.Animals.Where(a => a.location == "Africa").ToList();
+                Console.WriteLine(animal.name);
+                foreach(Animal i in animalsInAfrica) Console.WriteLine(i.name);
+            }
+            using (var context = new AnimalContext())
+            {
+                var animalToUpdate = context.Animals.Find(2);
+
+                // Modify the properties of the entity
+                animalToUpdate.name = "Giraffe";
+                context.SaveChanges();
+            }
+            using (var context = new AnimalContext())
+            {
+                var animalToDelete = context.Animals.Find(2);
+
+                // Remove the entity from the DbSet
+                context.Animals.Remove(animalToDelete);
+                context.SaveChanges();
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
